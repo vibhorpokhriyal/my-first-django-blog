aed382604accc4835cd7c8cee38fc9dd3cc22fde
vibhorpokhriyal <vibhorpokhriyal@gmail.com>
2018-02-28 07:26:49 +0530

    post sorting order changed to bring latest posts first

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
