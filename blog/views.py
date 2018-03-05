be0f950ae51f2dbfe4fff3b2871b6c788959ee8f
vibhorpokhriyal <vibhorpokhriyal@gmail.com>
2018-03-05 18:41:11 +0530

    made the code pep8 compliant

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,18 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 # Create your views here.
+
+
 def post_list(request):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('-publish_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by
+    ('-publish_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post':post})
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == 'POST':
@@ -21,10 +26,11 @@ def post_new(request):
             post.author = request.user
             post.publish_date = timezone.now()
             post.save()
-            return redirect('post_detail',pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
